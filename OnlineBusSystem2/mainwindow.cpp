148579cfce892ae62cc602532bf166898d7844f4
minwo324 <aprillee2412720@gmail.com>
2014-04-14 03:51:41 +0800

    add connection

--- OnlineBusSystem2/mainwindow.cpp
+++ OnlineBusSystem2/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "bussimulator.h"
 #include "busstop.h"
+#include "imagewidget.h"
 #include <QtDebug>
 #include <iostream>
 #include <stdio.h>
@@ -16,23 +17,25 @@ MainWindow::MainWindow(QWidget *parent)
     createBusStopList();
     createBusTimePanel();
     createMapPanel();
+    createAction();
 
     QWidget *window = new QWidget();
     window->setLayout(mainWindowLayout);
 
     setCentralWidget(window);
+
+    myTimerId=0;
+    timerCount=0;
+
+    bs=new BusSimulator();
+    routeSelected = "A1";
+    isChecked = false;
+    emit routeGet(routeSelected, getRoute(routeSelected));	
 }
 
 MainWindow::~MainWindow()
 {
 
-}
-
-void MainWindow::initialize(BusSimulator **bs)
-{
-    myTimerId=0;
-    timerCount=0;
-    this->bs=bs;
 }
 
 //event overwrite
@@ -54,22 +57,22 @@ void MainWindow::timerEvent(QTimerEvent *event)
         //dispatch buses
         if(timerCount>=6)
         {
-            (*bs)->dispatchBus("A1");
-            (*bs)->dispatchBus("A2");
-            (*bs)->dispatchBus("B");
-            (*bs)->dispatchBus("C");
-            (*bs)->dispatchBus("D1");
-            (*bs)->dispatchBus("D2");
+            bs->dispatchBus("A1");
+            bs->dispatchBus("A2");
+            bs->dispatchBus("B");
+            bs->dispatchBus("C");
+            bs->dispatchBus("D1");
+            bs->dispatchBus("D2");
             timerCount=0;
         }
 
         //get bus location
-        int *a1position=(*bs)->getBusPosition("A1");
-        int *a2position=(*bs)->getBusPosition("A2");
-        int *bposition=(*bs)->getBusPosition("B");
-        int *cposition=(*bs)->getBusPosition("C");
-        int *d1position=(*bs)->getBusPosition("D1");
-        int *d2position=(*bs)->getBusPosition("D2");
+        int *a1position=bs->getBusPosition("A1");
+        int *a2position=bs->getBusPosition("A2");
+        int *bposition=bs->getBusPosition("B");
+        int *cposition=bs->getBusPosition("C");
+        int *d1position=bs->getBusPosition("D1");
+        int *d2position=bs->getBusPosition("D2");
 
         //print all bus location
         qDebug()<<"A1=================================================";
@@ -85,12 +88,12 @@ void MainWindow::timerEvent(QTimerEvent *event)
                 //qDebug("[] ");
         }
         qDebug()<<a1map;
-        QVector<int> time=(*bs)->getTiming("House 7", false);
-        QVector<int> nextTime= (*bs)->getTiming("House 7",true);
-        QVector<int> occupancy= (*bs)->getOccupancy("House 7");
-        QVector<int> occupancyLimit=(*bs)->getOccupancyLimit("House 7");
-        QVector<QString> service=(*bs)->getService("House 7");
-        qDebug()<<"House 7 crowdedness: "<<(*bs)->getCrowdedness("House 7");
+        QVector<int> time=bs->getTiming("House 7", false);
+        QVector<int> nextTime= bs->getTiming("House 7",true);
+        QVector<int> occupancy= bs->getOccupancy("House 7");
+        QVector<int> occupancyLimit=bs->getOccupancyLimit("House 7");
+        QVector<QString> service=bs->getService("House 7");
+        qDebug()<<"House 7 crowdedness: "<<bs->getCrowdedness("House 7");
         for(int i=0;i<service.count();i++)
         {
             if(occupancyLimit[i]>60)
@@ -113,12 +116,12 @@ void MainWindow::timerEvent(QTimerEvent *event)
                 //qDebug("[] ");
         }
         qDebug()<<a2map;
-        time=(*bs)->getTiming("Opp KR MRT Station", false);
-        nextTime= (*bs)->getTiming("Opp KR MRT Station",true);
-        occupancy= (*bs)->getOccupancy("Opp KR MRT Station");
-        occupancyLimit=(*bs)->getOccupancyLimit("Opp KR MRT Station");
-        service=(*bs)->getService("Opp KR MRT Station");
-        qDebug()<<"Opp KR MRT Station crowdedness: "<<(*bs)->getCrowdedness("Opp KR MRT Station");
+        time=bs->getTiming("Opp KR MRT Station", false);
+        nextTime= bs->getTiming("Opp KR MRT Station",true);
+        occupancy= bs->getOccupancy("Opp KR MRT Station");
+        occupancyLimit=bs->getOccupancyLimit("Opp KR MRT Station");
+        service=bs->getService("Opp KR MRT Station");
+        qDebug()<<"Opp KR MRT Station crowdedness: "<<bs->getCrowdedness("Opp KR MRT Station");
         for(int i=0;i<service.count();i++)
         {
             if(occupancyLimit[i]>60)
@@ -127,16 +130,21 @@ void MainWindow::timerEvent(QTimerEvent *event)
                 qDebug()<<service[i]<<" occupancy:"<<occupancy[i]<<"/"<<occupancyLimit[i]<<" next bus:"<<time[i]<<" "<<nextTime[i];
         }
 
-        //(*bs)->advanceAllBus();
-        //(*bs)->addCrowd();
+        bs->advanceAllBus();
+        bs->addCrowd();
         timerCount++;
-
     }
     else
         QWidget::timerEvent(event); // propagate
 
 }
 
+void MainWindow::createAction(){
+	connect(busList, SIGNAL(activated(const QString &)), 
+		this, SLOT(selectRoute(const QString &)));
+	
+	connect(this, SIGNAL(routeGet(const QString &, BusStop**)),imageWidget, SLOT(displayRoute(const QString &, BusStop**)));
+}
 //GUI
 void MainWindow::createTitleBar()
 {
@@ -159,14 +167,100 @@ void MainWindow::createTitleBar()
 
 void MainWindow::createBusStopList()
 {
-    busStopListPanel = new BusStopListPanel();
-    mainWindowLayout->addWidget(busStopListPanel,0,0);
+    busStopSearchColumn = new QHBoxLayout();
+    busStopObject = new QHBoxLayout();
+    busStopListLayout = new QVBoxLayout();
+    busStopList = new QListWidget();
+
+    searchBar = new QLineEdit();
+    searchButton = new QPushButton("Search");
+    busStopLabel = new QLabel("Science"); //test
+    busIcon = new QLabel("bus"); //test
+
+    busStopSearchColumn->addWidget(searchBar);
+    busStopSearchColumn->addWidget(searchButton);
+
+    busStopObject->addWidget(busStopLabel);
+    busStopObject->addWidget(busIcon);
+
+    //busStopListScroll->addWidget(busStopObject);
+    /*
+    busStopList->addLayout(busStopObject);
+    busStopList->addLayout(busStopObject);
+    busStopList->addLayout(busStopObject);
+    busStopList->addLayout(busStopObject);
+    busStopList->addLayout(busStopObject);
+    */
+
+    busStopList->addItem(new QListWidgetItem("Science"));
+    busStopList->addItem(new QListWidgetItem("Science"));
+    busStopList->addItem(new QListWidgetItem("Science"));
+    busStopList->addItem(new QListWidgetItem("Science"));
+    busStopList->addItem(new QListWidgetItem("Science"));
+
+    busStopList->setItemWidget(new QListWidgetItem("Science"),busIcon);
+
+    busStopList->setFixedHeight(400);
+    //busStopList->setLayout(busStopObject);
+
+    busStopListLayout->addLayout(busStopSearchColumn);
+    busStopListLayout->addWidget(busStopList);
+
+    mainWindowLayout->addLayout(busStopListLayout,0,0);
+
+/*
+    QWidget *window = new QWidget();
+    window->setLayout(busStopListLayout);
+
+    setCentralWidget(window);
+*/
 }
 
 void MainWindow::createBusTimePanel()
-{
-    waitTimeWidget = new WaitTimeWidget();
-    mainWindowLayout->addWidget(waitTimeWidget,1,0);
+{        
+    QLabel *nextBusTime;
+    QLabel *nextTwoBusTime;
+    QLabel *minLabel;
+
+    timePanel = new QHBoxLayout();
+    leftPanel = new QVBoxLayout();
+    estimateTime = new QVBoxLayout();
+    occupancyBox = new QHBoxLayout();
+    timeObject = new QHBoxLayout();
+    timeObject2 = new QHBoxLayout();
+
+    busName = new QLabel("A1");
+    occupancyLabel = new QLabel("Occupancy: ");
+    vacancyLabel = new QLabel("40");
+    slashLabel = new QLabel("/");
+    busSeatLabel = new QLabel("60");
+
+    occupancyBox->addWidget(occupancyLabel);
+    occupancyBox->addWidget(vacancyLabel);
+    occupancyBox->addWidget(slashLabel);
+    occupancyBox->addWidget(busSeatLabel);
+
+    nextBusTime = new QLabel("7");
+    nextTwoBusTime = new QLabel("20");
+    //nextTwoBusTime = new QLabel("22");
+    minLabel = new QLabel("min");
+
+    timeObject->addWidget(nextBusTime);
+    timeObject->addWidget(minLabel);
+
+    timeObject2->addWidget(nextTwoBusTime);
+    timeObject2->addWidget(minLabel);
+
+    leftPanel->addWidget(busName);
+    leftPanel->addLayout(occupancyBox);
+
+    estimateTime->addLayout(timeObject);
+    estimateTime->addLayout(timeObject2);
+
+    timePanel->addLayout(leftPanel);
+    timePanel->addLayout(estimateTime);
+
+    mainWindowLayout->addLayout(timePanel,1,0);
 }
 
 void MainWindow::createMapPanel()
@@ -179,16 +273,59 @@ void MainWindow::createMapPanel()
     busList->setFixedWidth(100);
     busList->addItems(buses);
 
-    mapLabel = new QLabel("Map is here.");
-
+    //mapLabel = new QLabel("Map is here.");
     //mapLabel->setFixedHeight(300);
-    mapLabel->setFixedWidth(800);
+    //mapLabel->setFixedWidth(800);
+	imageWidget = new ImageWidget;
+	imageWidget->setFixedHeight(600);
+    imageWidget->setFixedWidth(900);
 
     mapPanel->addWidget(busList);
-    mapPanel->addWidget(mapLabel);
-
+    mapPanel->addWidget(imageWidget);
 
     mainWindowLayout->addLayout(mapPanel,0,1);
-
-    //mainWindowLayout->addWidget(waitTimeWidget,0,1);
 }
+
+//slot methods
+void MainWindow::selectRoute(const QString &route){
+	routeSelected = route;
+	if (isChecked){
+		getLocation(routeSelected);
+		qDebug("check location!");
+	} else {
+		qDebug("get route information!");
+		emit routeGet(route, getRoute(routeSelected));		
+	}
+}
+
+//methods
+BusStop** MainWindow::getRoute(const QString &route){
+	//qDebug ("Checking route.....");
+	BusStop** result = bs->getRoute(route);
+	//qDebug()<< result[0]->getName();
+	//qDebug()<< result[1]->getName();
+	//qDebug()<< result[2]->getName();
+	return result;
+	
+}
+
+void MainWindow::getLocation(const QString &route){
+	int *result = bs->getBusPosition(route);
+	int index;
+	/*for (int i=0;i<length(result);i++){
+		return;
+	}*/
+	return;
+}
+
+int MainWindow::getCount(const QString &route){
+	int count;
+	if (route == "A1")
+		count = 16;
+	else if (route == "A2")
+		count = 17;
+	else if (route == "D1")
+		count = 14;
+	else
+		count = 12;
+} 
--- OnlineBusSystem2/mainwindow.h
+++ OnlineBusSystem2/mainwindow.h
@@ -2,13 +2,12 @@
 #define MAINWINDOW_H
 
 #include "bussimulator.h"
-#include "waittimewidget.h"
-#include "busstoplistpanel.h"
 #include <QtGui>
 
 class Bus;
 class BusStop;
 class BusSimulator;
+class ImageWidget;
 
 class MainWindow : public QMainWindow
 {
@@ -17,15 +16,21 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    void initialize(BusSimulator **bs);
-    bool updateAllowed();
-    void toggleUpdate();
+
+signals:
+	void routeGet(const QString &str, BusStop** result);
+	
+public slots:
+	void selectRoute(const QString &str);
 
 private:
     void createTitleBar();
     void createBusStopList();
     void createBusTimePanel();
     void createMapPanel();
+    void createAction();
+    BusStop** getRoute(const QString &str);
+    void getLocation(const QString &str);
 
     QGridLayout *mainWindowLayout;
 
@@ -36,24 +41,51 @@ private:
     QHBoxLayout *titleLayout;
 
     //bus stop list panel
-    BusStopListPanel *busStopListPanel;
+    QLineEdit *searchBar;
+    QPushButton *searchButton;
+    QVBoxLayout *busStopListLayout;
+    QListWidget *busStopList;
+    QHBoxLayout *busStopSearchColumn;
+    QHBoxLayout *busStopObject;
+    QScrollArea *busStopListScroll;
+    QLabel *busStopLabel;
+    QLabel *busIcon;
 
     //time panel
-    WaitTimeWidget *waitTimeWidget;
+    QHBoxLayout *timePanel;
+    QVBoxLayout *leftPanel;
+    QVBoxLayout *estimateTime;
+
+    QHBoxLayout *occupancyBox;
+    QHBoxLayout *timeObject;
+    QHBoxLayout *timeObject2;
+
+    QLabel *busName;
+    QLabel *occupancyLabel;
+    QLabel *vacancyLabel;
+    QLabel *slashLabel;
+    QLabel *busSeatLabel;
+
+    QLabel *nextBusTime;
+    QLabel *nextTwoBusTime;
+    QLabel *minLabel;
 
     //map panel
     QComboBox *busList;
     QLabel *mapLabel;
+    ImageWidget *imageWidget;
 
 
     int myTimerId;
-    BusSimulator **bs;
+    BusSimulator *bs;
     int timerCount;
+	QString routeSelected;
+	bool isChecked;
 
-    protected:
-        void timerEvent(QTimerEvent *event);
-        void showEvent(QShowEvent *event);
-        void hideEvent(QHideEvent *event);
+protected:
+    void timerEvent(QTimerEvent *event);
+    void showEvent(QShowEvent *event);
+    void hideEvent(QHideEvent *event);
 
 };
 
